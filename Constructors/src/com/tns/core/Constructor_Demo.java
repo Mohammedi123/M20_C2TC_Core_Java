29d96064f1c8d6f96abe11f969ccbe85ce85f8bb
cw user <cw user@DESKTOP-H2HKVB1>
2022-06-22 13:49:10 -0700

    First Commit

---
+++ Constructors/src/com/tns/core/Constructor_Demo.java
@@ -0,0 +1,20 @@
+package com.tns.core;
+
+public class Constructor_Demo {
+	
+	public Constructor_Demo() {
+		System.out.println("Constructor executed");
+	}
+
+	public static void main(String[] args) 
+	{
+		Constructor_Demo obj1=new Constructor_Demo();
+		Constructor_Demo obj2=new Constructor_Demo();
+		Constructor_Demo obj3=new Constructor_Demo();
+		Constructor_Demo obj4=new Constructor_Demo();
+		Constructor_Demo obj5=new Constructor_Demo();
+		
+
+	}
+
+}
