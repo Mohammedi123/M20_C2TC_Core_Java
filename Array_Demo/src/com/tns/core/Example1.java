edbc588f369195b6aeb1fb44601d802fa0e447fd
cw user <cw user@DESKTOP-H2HKVB1>
2022-06-29 13:42:28 -0700

    First Commit

---
+++ Array_Demo/src/com/tns/core/Example1.java
@@ -0,0 +1,24 @@
+package com.tns.core;
+
+public class Example1 
+{
+	
+	public static void main(String[] args) {
+	                             	// one dimensional Array//
+		float[] ar=new float[10];
+		short[] ab=new short[3];
+		byte[] ac=new byte[3];
+		double[] ad=new double[3];
+		boolean[] ae=new boolean[3];
+		long[] al=new long[3];
+		
+		System.out.println(ar.getClass().getName());
+		System.out.println(ab.getClass().getName());
+		System.out.println(ac.getClass().getName());
+		System.out.println(ad.getClass().getName());
+		System.out.println(ae.getClass().getName());
+		System.out.println(al.getClass().getName());
+		
+	}
+
+}
