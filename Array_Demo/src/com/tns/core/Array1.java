22d3af0fd9deca48cb0afd4093c8639a7d94bfdd
cw user <cw user@DESKTOP-H2HKVB1>
2022-07-28 19:33:10 -0700

    First Commit

---
+++ Array_Demo/src/com/tns/core/Array1.java
@@ -0,0 +1,12 @@
+package com.tns.core;
+
+//arrays
+public class Array1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float[] a=new float[3];
+		System.out.println(a.getClass().getName()); 
+	}
+
+}
---
+++ Array_Demo/src/com/tns/core/Array3.java
@@ -0,0 +1,17 @@
+package com.tns.core;
+
+//fetching values of array using for loop
+
+public class Array3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+     int a []= {1,2,3,4,5};
+    for(int i=0;i<5;i++)
+{
+      System.out.println(a[i]);
+}
+}
+
+}
