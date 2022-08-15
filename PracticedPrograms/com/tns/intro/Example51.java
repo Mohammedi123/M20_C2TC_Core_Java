66e6ebd91479873cfebf4ed7a6127b9cfcaf8c49
Mohammedi123 <103200300+Mohammedi123@users.noreply.github.com>
2022-08-16 01:17:26 +0530

    First Commit

---
+++ PracticedPrograms/com/tns/intro/Example51.java
@@ -0,0 +1,17 @@
+package com.tns.intro;
+
+import java.util.Scanner;
+
+public class Example51 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the value:");
+		double d= scan.nextDouble();
+		System.out.println(" Entered value is:" +d);
+		scan.close();
+		
+
+	}
+
+}
---
+++ PracticedPrograms/com/tns/intro/Example61.java
@@ -0,0 +1,17 @@
+package com.tns.intro;
+
+import java.util.Scanner;
+
+public class Example61 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the String:");
+		String str=scan.nextLine();
+		System.out.println("Entered String is:" +str);
+		scan.close();
+		
+
+	}
+
+}
