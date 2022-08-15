f2b5cdaf19a8b90614f600e38eebe0faf2bf43f0
Mohammedi123 <103200300+Mohammedi123@users.noreply.github.com>
2022-08-16 01:32:48 +0530

    First Commit

---
+++ PracticedPrograms/Example3.java
@@ -0,0 +1,12 @@
+package com.tns.intro;
+
+import java.util.Scanner;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		char c='A';
+		System.out.println("Character is :" +c);
+
+	}
+}
---
+++ PracticedPrograms/Example6.java
@@ -0,0 +1,11 @@
+package com.tns.intro;
+
+public class Example6 {
+
+	public static void main(String[] args) {
+		String str="Mohammedi";
+		System.out.println(str);
+
+	}
+
+}
